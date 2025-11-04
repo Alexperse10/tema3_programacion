31fde48ae99728fdd594c6e53d6d1eab5e823f2a
AlexPerse10 <spuknitfire@gmail.com>
2025-11-04 13:04:11 +0100

    comienzo de ejercicios arrays crear y mostrar

---
+++ ejercicios_casa_arrays/ejercicios_arrays_casa/src/main/java/org/example/ejercicios_1_crear_mostrar.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ejercicios_1_crear_mostrar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
