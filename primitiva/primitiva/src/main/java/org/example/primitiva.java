159231f6060653863272fbd50b1a0a79a8f5b50e
AlexPerse10 <spuknitfire@gmail.com>
2025-11-17 14:32:31 +0100

    se avanza en la practica de primitiva

--- primitiva/primitiva/src/main/java/org/example/primitiva.java
+++ primitiva/primitiva/src/main/java/org/example/primitiva.java
@@ -10,43 +10,65 @@ public class primitiva {
         Random aleatorio = new Random();
 
 
-        int [] reintegro = new int[1];
-        int [] complementario = new int[1];
-        int [] numero_completo = new int[8];
+        int[] reintegro = new int[1];
+        int[] complementario = new int[1];
+        int[] numero_completo = new int[8];
         boolean repetir = false;
-        int [] sorteonumero = new int[6]; // creo un vector para realizar el sorteo, este vector lo rellono con los numeros aleatorios
+        int[] sorteonumero = new int[6]; // creo un vector para realizar el sorteo, este vector lo rellono con los numeros aleatorios
 
-            System.out.println("introduce los datos de tu boleto");
-            String numero_complete = sc.nextLine();
-                // se valida el formato
-                if (numero_complete.matches("\\d{1}-\\d{1}-\\d{1}-\\d{1}-\\d{1}-\\d{1}/\\d{1}")) {
-                    System.out.println("formato correcto");
-                    //aqui creo el array
-                    String [] numero_fragmentado = numero_complete.split("[-/]"); // lo creo en partes
-                    System.out.println(Arrays.toString(numero_fragmentado)); // miro como queda el array
-                    int [] numero = new int[numero_fragmentado.length];  // creo un array de la longitud del numero de tipo int
+        System.out.println("introduce los datos de tu boleto");
+        String numero_complete = sc.nextLine();
+        // se valida el formato
+        if (numero_complete.matches("\\d{1}-\\d{1}-\\d{1}-\\d{1}-\\d{1}-\\d{1}/\\d{1}")) {
+            System.out.println("formato correcto");
+            //aqui creo el array
+            String[] numero_fragmentado = numero_complete.split("[-/]"); // lo creo en partes
+            System.out.println(Arrays.toString(numero_fragmentado)); // miro como queda el array
+            int[] numero = new int[numero_fragmentado.length];  // creo un array de la longitud del numero de tipo int
 
-                    for (int i =0; i<numero_fragmentado.length; i++){ //convertir de strings a numeros enteros.
-                        numero [i] = Integer.parseInt(numero_fragmentado[i]); //los siete numeros
+            for (int i = 0; i < numero_fragmentado.length; i++) { //convertir de strings a numeros enteros.
+                numero[i] = Integer.parseInt(numero_fragmentado[i]); //los siete numeros
 
-                    }
+            }
 
-                    int [] seisnumeros = new int [6]; // creo el array para los primeros seis numeros, este array esta vacio
+            int[] seisnumeros = new int[6]; // creo el array para los primeros seis numeros, este array esta vacio
 
-                    for (int i =0; i<6; i++){
-                        seisnumeros [i] = numero[i]; // aqui hago un copia pega de los primeros 6 numeros
-                    }
-                    reintegro [0] = numero [6];  // el sexto numero es el reintegro.
-                    for (int i =0; i<sorteonumero.length; i++){  // cuidado hay que hacerlo con el sorteo ya es hasta seis
-                        sorteonumero [i] = aleatorio.nextInt(49)+1;
-                     }
-                    sorteonumero = Arrays.stream(sorteonumero).distinct().toArray();
+            for (int i = 0; i < 6; i++) {
+                seisnumeros[i] = numero[i]; // aqui hago un copia pega de los primeros 6 numeros
+            }
+            //reintegro[0] = numero[6];  // el sexto numero es el reintegro.
 
-                }else {
-                    System.out.println("saliendo del programa...");
+            do {
+                for (int i = 0; i < sorteonumero.length; i++) {  // cuidado hay que hacerlo con el sorteo ya es hasta seis, despues con reintegro
+                    sorteonumero[i] = aleatorio.nextInt(49) + 1; //desde uno hasta 49
+
+
+                }
+                sorteonumero = Arrays.stream(sorteonumero).distinct().toArray();
+                System.out.println(Arrays.toString(sorteonumero));
+            } while (sorteonumero.length < 6);
+            Arrays.sort(sorteonumero);
+            System.out.println(Arrays.toString(sorteonumero));
+            int[] sorteocomplementario = new int[1];
+            do {
+
+                for (int i = 0; i < sorteocomplementario.length; i++) {
+                    sorteocomplementario[i] = aleatorio.nextInt(49)+1;
+                }
+                sorteocomplementario = Arrays.stream(sorteonumero).distinct().toArray();
+
+            } while (sorteocomplementario.length < 0) ;
+
+                for (int i =0; i< sorteocomplementario.length; i++){
+                    reintegro [i] = aleatorio.nextInt(9);
                 }
 
 
+            }else{
+                System.out.println("saliendo del programa...");
+            }
 
+
+        }
     }
-}
+
