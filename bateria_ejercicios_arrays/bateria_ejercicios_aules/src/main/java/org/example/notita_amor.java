0441f1ddd90a894fe78dbcf003bc2058b6b7a788
AlexPerse10 <spuknitfire@gmail.com>
2025-11-21 14:28:41 +0100

    ok

---
+++ bateria_ejercicios_arrays/bateria_ejercicios_aules/src/main/java/org/example/notita_amor.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class notita_amor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int mapa[][] = new int[6][4];
+        int profesor;
+        int notita;
+        int estudiante=0;
+        Random aleatorio = new Random();
+
+      mapa [0][0] = 3;
+      mapa [1][0] = 2;
+      mapa [0][1] = 2;
+      mapa [1][1] = 1;
+      mapa [1][2] = 1;
+      mapa [0][2] = 0; 
+
+
+        }
+    }
